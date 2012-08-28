62e1acbb72ce7ca1c98ab2e93bb88bc37a62872a
abdul <abdul@mongolab.com>
2012-08-28 15:27:17 -0700

    Adding __init__ and setup

---
+++ setup.py
@@ -0,0 +1,42 @@
+# The MIT License
+
+# Copyright (c) 2012 ObjectLabs Corporation
+
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+from setuptools import setup
+
+setup(
+    name='pymaker',
+    version='0.2.0',
+    author='MongoLab Team',
+    author_email='team@mongolab.com',
+    description='python maker',
+    long_description="python maker",
+    packages=['pymaker'],
+    url='https://github.com/objectlabs/pymaker',
+    license='MIT',
+    install_requires=[
+        'pymongo==2.1.1',
+    ],
+    dependency_links=[
+        "https://github.com/dampier/mongo-python-driver/tarball/master#egg=dampier-pymongo-2.1.1"
+    ]
+)
